90509ce262ecae10773572f3257e2887bdeb91d4
ammustephy <ammustephy.as@gmail.com>
2024-02-26 10:57:26 +0530

    first commit

---
+++ bin/dart_pgm_1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_pgm_1/dart_pgm_1.dart' as dart_pgm_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_pgm_1.calculate()}!');
+}
---
+++ bin/sample.dart
@@ -0,0 +1,10 @@
+void main(){
+  String fname= 'Stephy', email='ammustephy.as@gmail.com';
+  int age= 25;
+  double cgpa=7.2;
+  print('Name is : $fname');
+  print('Email id is : $email');
+  print('age is : $age');
+  print('cgpa : $cgpa');
+
+}
